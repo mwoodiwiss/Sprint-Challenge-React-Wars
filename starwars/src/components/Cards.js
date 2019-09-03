fd625cde5bc77fefd6fd5f15b06d8715589084ab
mwoodiwiss <malachaiwoodiwiss1999@gmail.com>
2019-09-03 17:31:42 -0600

    fixed-styling-meatreached-mvp

--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react';
 import './CardsDiv.css';
-
 import './CardsDiv.scss';
-// import { Card } from 'semantic-ui-react';
 
 
 export function ACard(props) {
     return (
-        <div className='card'>
+        <Card className='card'>
             <h1>{props.name}</h1>
             <p>{'Gender: '+props.gender}</p>
             <p>{'Birthday: '+props.birth}</p>
             <p>{'Height: '+props.height}</p>
             <p>{'Eye Color: '+props.eyes}</p>
-        </div>
+        </Card>
     )
 }
